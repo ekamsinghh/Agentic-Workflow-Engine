f35afec292ef1b4978540dd5700efafa5b047b0b
Ekam Singh <ekamsinghh24@gmail.com>
2026-08-24 20:19:38 +0530

    Added database connection and functions

---
+++ src/database/sqlite.py
@@ -0,0 +1,37 @@
+import sqlite3 as sq
+
+def init_db(db_path: str = "checkpoints.sqlite"):
+    connection = sq.connect(db_path) # connection object to the disk based local database
+
+    cur = connection.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS checkpoints (run_id TEXT PRIMARY KEY,current_node TEXT,state_data TEXT)"
+    )
+    res = cur.execute("SELECT name from sqlite_master")
+    if(res.fetchone() is None):
+        print("Error in creating table⚠️")
+
+    connection.close()
+
+def save_checkpoint(run_id: str, current_node: str, state_json: str, db_path: str = "checkpoints.sqlite"):
+
+    connection = sq.connect(db_path)
+    cur = connection.cursor()
+
+    cur.execute(
+        "INSERT OR REPLACE INTO checkpoints (run_id, current_node, state_data)VALUES (?, ?, ?)",(run_id,current_node,state_json)
+    )
+    connection.commit() # to commit the current transaction
+    connection.close()
+
+def load_checkpoint(run_id: str,db_path: str = "checkpoints.sqlite") -> (tuple[str,str] | None): # tuple[str,str] ,eans the function will return tuple of size 2 with 2 string entries
+    connection = sq.connect(db_path)
+    cur = connection.cursor()
+
+    res = cur.execute(
+        "SELECT current_node,state_data FROM checkpoints where run_id = ?",(run_id,)
+    )
+
+    loaded_data = res.fetchone() # None if no saved checkpoints
+    connection.close()
+    return loaded_data
---
+++ src/engine.py
@@ -0,0 +1,35 @@
+from .schema import Node, WorkflowState
+
+class Graph:
+    def __init__(self):
+        self.nodes: dict[str,Node] = {}
+        self.edges: dict[str,dict[str,Node]] = {}
+        self.entry_point: str | None = None
+
+    def add_node(self, node: Node):
+        self.nodes[node.name] = node
+
+    def set_entry_point(self,node_name: str):
+        self.entry_point = node_name
+
+    def add_edge(self,source_name: str, end: Node,condition: str):
+        if(source_name not in self.edges):
+            self.edges[source_name] = {}
+
+        self.edges[source_name][condition] = end
+
+    def run(self,state: WorkflowState):
+        current_node = self.entry_point
+
+        while(current_node is not None):
+            node = self.nodes[current_node]
+            state = node.operation(state)
+
+            state.history.append(current_node)
+            node_edges = self.edges.get(node.name,{}) # using get method so that if the key is absent then it will not throw an error
+            if(state.status in node_edges):
+                current_node = node_edges[state.status]
+            else:
+                current_node = None
+
+        return state
